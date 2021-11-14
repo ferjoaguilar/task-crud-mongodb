f3ab1bf8cca188d08eedebd3ca87378f07a412c2
Fernando Aguilar <feraguilar6985@gmail.com>
2021-11-14 16:00:01 -0600

    feat(middleware):add middleware erro to service

--- services/task.service.ts
+++ services/task.service.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'bson';
 import { Itask } from "../interfaces/Itasks.interface";
 import database from "../libs/database.libs";
+import HttpException from '../libs/exceptions.libs';
 
 class Tasks {
   async created(task:Itask) {
@@ -17,6 +18,9 @@ class Tasks {
   async getTask(id: string) {
     const params = new ObjectId(id)
     const findResult = await (await database('tasks')).findOne({ _id: params })
+    if (findResult === null) {
+      throw new HttpException(404, 'Taks not found')
+    }
     return findResult
   }
 
@@ -24,12 +28,18 @@ class Tasks {
     const params = new ObjectId(id)
     const updateResult = await (await database('tasks'))
     .updateOne({_id: params}, {$set: {title: task.title, description: task.description}})
+    if (updateResult.upsertedId === null) {
+      throw new HttpException(404, 'Taks not found')
+    }
     return updateResult
   }
 
   async delete(id:string) {
     const params = new ObjectId(id)
     const deleteResult = await (await database('tasks')).deleteOne({_id: params})
+    if (deleteResult.deletedCount === 0) {
+      throw new HttpException(404, 'Task not found')
+    }
     return deleteResult
   }
 }
